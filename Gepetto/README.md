44b16e5b1c561dff22d0003eab079d262afcca3e
AnonymousUser <you@example.com>
2022-12-06 02:13:52 +0000

    Update

---
+++ Gepetto/README.md
@@ -0,0 +1,51 @@
+# Gepetto
+
+Gepetto is a Python script which uses OpenAI's davinci-003 model to provide meaning to functions decompiled by IDA Pro.
+At the moment, it can ask davinci-003 to explain what a function does, and to automatically rename its variables.
+Here is a simple example of what results it can provide in mere seconds:
+
+![](https://github.com/JusticeRage/Gepetto/blob/main/readme/comparison.png?raw=true)
+
+## Setup
+
+Simply drop this script into your IDA plugins folder (`$IDAUSR/plugins`).
+
+You will need to add the required packages to IDA's Python installation for the script to work.
+Find which interpreter IDA is using by checking the following registry key: 
+`Computer\HKEY_CURRENT_USER\Software\Hex-Rays\IDA` (default on Windows: `%LOCALAPPDATA%\Programs\Python\Python39`).
+Finally, with the corresponding interpreter, simply run: 
+
+```
+[/path/to/python] -m pip install -r requirements.txt
+```
+
+⚠️ You will also need to edit the script and add your own API key, which can be found on [this page](https://beta.openai.com/account/api-keys).
+Please note that davinci-003 queries are not free (although not very expensive) and you will need to setup a payment method.
+
+## Usage
+
+Once the plugin is installed properly, you should be able to invoke it from the context menu of IDA's pseudocode window,
+as shown in the screenshot below:
+
+![](https://github.com/JusticeRage/Gepetto/blob/main/readme/usage.png?raw=true)
+
+You can also use the following hotkeys:
+
+- Ask davinci-003 to explain the function: `Ctrl` + `Alt` + `H`
+- Request better names for the function's variables: `Ctrl` + `Alt` + `R`
+
+Initial testing shows that asking for better names works better if you ask for an explanation of the function first – I
+assume because davinci-003 then uses its own comment to make more accurate suggestions.
+There is an element of randomness to the AI's replies. If for some reason the initial response you get doesn't suit you,
+you can always run the command again.
+
+## Limitations
+
+- The plugin requires access to the HexRays decompiler to function.
+- davinci-003 is a general-purpose language model and may very well get things wrong! Always be critical of results returned!
+
+## Acknowledgements
+
+- [OpenAI](https://openai.com), for making this incredible chatbot, obviously
+- [Hex Rays](https://hex-rays.com/), the makers of IDA for their lightning fast support
+- [Kaspersky](https://kaspersky.com), for funding all my research
---
+++ Gepetto/gepetto.py
@@ -0,0 +1,236 @@
+import functools
+import json
+import idaapi
+import ida_hexrays
+import ida_kernwin
+import idc
+import openai
+import os
+import re
+import textwrap
+import threading
+
+# Set your API key here, or put in in the OPENAI_API_KEY environment variable.
+openai.api_key = ""
+
+# =============================================================================
+# Setup the context menu and hotkey in IDA
+# =============================================================================
+
+class GepettoPlugin(idaapi.plugin_t):
+    flags = 0
+    explain_action_name = "gepetto:explain_function"
+    explain_menu_path = "Edit/Gepetto/Explain function"
+    rename_action_name = "gepetto:rename_function"
+    rename_menu_path = "Edit/Gepetto/Rename variables"
+    wanted_name = 'Gepetto'
+    wanted_hotkey = ''
+    comment = "Uses davinci-003 to enrich the decompiler's output"
+    help = "See usage instructions on GitHub"
+    menu = None
+
+    def init(self):
+        # Check whether the decompiler is available
+        if not ida_hexrays.init_hexrays_plugin():
+            return idaapi.PLUGIN_SKIP
+
+        # Function explaining action
+        explain_action = idaapi.action_desc_t(self.explain_action_name,
+                                              'Explain function',
+                                              ExplainHandler(),
+                                              "Ctrl+Alt+G",
+                                              'Use davinci-003 to explain the currently selected function',
+                                              199)
+        idaapi.register_action(explain_action)
+        idaapi.attach_action_to_menu(self.explain_menu_path, self.explain_action_name, idaapi.SETMENU_APP)
+
+        # Variable renaming action
+        rename_action = idaapi.action_desc_t(self.rename_action_name,
+                                             'Rename variables',
+                                             RenameHandler(),
+                                             "Ctrl+Alt+R",
+                                             "Use davinci-003 to rename this function's variables",
+                                             199)
+        idaapi.register_action(rename_action)
+        idaapi.attach_action_to_menu(self.rename_menu_path, self.rename_action_name, idaapi.SETMENU_APP)
+
+        # Register context menu actions
+        self.menu = ContextMenuHooks()
+        self.menu.hook()
+
+        return idaapi.PLUGIN_KEEP
+
+    def run(self, arg):
+        pass
+
+    def term(self):
+        idaapi.detach_action_from_menu(self.explain_menu_path, self.explain_action_name)
+        idaapi.detach_action_from_menu(self.rename_menu_path, self.rename_action_name)
+        if self.menu:
+            self.menu.unhook()
+        return
+
+# -----------------------------------------------------------------------------
+
+class ContextMenuHooks(idaapi.UI_Hooks):
+    def finish_populating_widget_popup(self, form, popup):
+        # Add actions to the context menu of the Pseudocode view
+        if idaapi.get_widget_type(form) == idaapi.BWN_PSEUDOCODE:
+            idaapi.attach_action_to_popup(form, popup, GepettoPlugin.explain_action_name, "Gepetto/")
+            idaapi.attach_action_to_popup(form, popup, GepettoPlugin.rename_action_name, "Gepetto/")
+
+# -----------------------------------------------------------------------------
+
+def comment_callback(address, view, response):
+    """
+    Callback that sets a comment at the given address.
+    :param address: The address of the function to comment
+    :param view: A handle to the decompiler window
+    :param response: The comment to add
+    """
+    # Add newlines at the end of each sentence.
+    response = "\n".join(textwrap.wrap(response, 80, replace_whitespace=False))
+
+    # Add the response as a comment in IDA.
+    idc.set_func_cmt(address, response, 0)
+    # Refresh the window so the comment is displayed properly
+    if view:
+        view.refresh_view(False)
+    print("davinci-003 query finished!")
+
+
+# -----------------------------------------------------------------------------
+
+class ExplainHandler(idaapi.action_handler_t):
+    """
+    This handler is tasked with querying davinci-003 for an explanation of the
+    given function. Once the reply is received, it is added as a function
+    comment.
+    """
+    def __init__(self):
+        idaapi.action_handler_t.__init__(self)
+
+    def activate(self, ctx):
+        decompiler_output = ida_hexrays.decompile(idaapi.get_screen_ea())
+        v = ida_hexrays.get_widget_vdui(ctx.widget)
+        query_model_async("Can you explain what the following C function does and suggest a better name for it?\n"
+                          + str(decompiler_output),
+                          functools.partial(comment_callback, address=idaapi.get_screen_ea(), view=v))
+        return 1
+
+    # This action is always available.
+    def update(self, ctx):
+        return idaapi.AST_ENABLE_ALWAYS
+
+# -----------------------------------------------------------------------------
+
+def rename_callback(address, view, response):
+    """
+    Callback that extracts a JSON array of old names and new names from the
+    response and sets them in the pseudocode.
+    :param address: The address of the function to work on
+    :param view: A handle to the decompiler window
+    :param response: The response from davinci-003
+    """
+    j = re.search(r"\{[^}]*?\}", response)
+    if not j:
+        print(f"Error: couldn't extract a response from davinci-003's output:\n{response}")
+        return
+    names = json.loads(j.group(0))
+
+    # The rename function needs the start address of the function
+    function_addr = idaapi.get_func(address).start_ea
+
+    replaced = []
+    for n in names:
+        if ida_hexrays.rename_lvar(function_addr, n, names[n]):
+            replaced.append(n)
+
+    # Update possible names left in the function comment
+    comment = idc.get_func_cmt(address, 0)
+    if comment and len(replaced) > 0:
+        for n in replaced:
+            comment = re.sub(r'\b%s\b' % n, names[n], comment)
+        idc.set_func_cmt(address, comment, 0)
+
+    # Refresh the window to show the new names
+    if view:
+        view.refresh_view(True)
+    print(f"davinci-003 query finished! {len(replaced)} variable(s) renamed.")
+
+# -----------------------------------------------------------------------------
+
+class RenameHandler(idaapi.action_handler_t):
+    """
+    This handler requests new variable names from davinci-003 and updates the
+    decompiler's output.
+    """
+    def __init__(self):
+        idaapi.action_handler_t.__init__(self)
+
+    def activate(self, ctx):
+        decompiler_output = ida_hexrays.decompile(idaapi.get_screen_ea())
+        v = ida_hexrays.get_widget_vdui(ctx.widget)
+        query_model_async("Analyze the following C function:\n" + str(decompiler_output) +
+                            "\nSuggest better variable names, reply with a JSON array where keys are the original names"
+                            "and values are the proposed names. Do not explain anything, only print the JSON "
+                            "dictionary.",
+                          functools.partial(rename_callback, address=idaapi.get_screen_ea(), view=v))
+        return 1
+
+    # This action is always available.
+    def update(self, ctx):
+        return idaapi.AST_ENABLE_ALWAYS
+
+# =============================================================================
+# davinci-003 interaction
+# =============================================================================
+
+def query_model(query, cb):
+    """
+    Function which sends a query to davinci-003 and calls a callback when the response is available.
+    Blocks until the response is received
+    :param query: The request to send to davinci-003
+    :param cb: Tu function to which the response will be passed to.
+    """
+    try:
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=query,
+            temperature=0.6,
+            max_tokens=2500,
+            top_p=1,
+            frequency_penalty=1,
+            presence_penalty=1,
+            timeout=60  # Wait 60 seconds maximum
+        )
+        ida_kernwin.execute_sync(functools.partial(cb, response=response.choices[0].text), ida_kernwin.MFF_WRITE)
+    except openai.OpenAIError as e:
+        print(f"davinci-003 could not complete the request: {str(e)}")
+    except Exception as e:
+        print(f"General exception encountered while running the query: {str(e)}")
+
+# -----------------------------------------------------------------------------
+
+def query_model_async(query, cb):
+    """
+    Function which sends a query to davinci-003 and calls a callback when the response is available.
+    :param query: The request to send to davinci-003
+    :param cb: Tu function to which the response will be passed to.
+    """
+    print("Request to davinci-003 sent...")
+    t = threading.Thread(target=query_model, args=[query, cb])
+    t.start()
+
+# =============================================================================
+# Main
+# =============================================================================
+
+def PLUGIN_ENTRY():
+    if not openai.api_key:
+        openai.api_key = os.getenv("OPENAI_API_KEY")
+        if not openai.api_key:
+            print("Please edit this script to insert your OpenAI API key!")
+            raise ValueError("No valid OpenAI API key found")
+
+    return GepettoPlugin()
